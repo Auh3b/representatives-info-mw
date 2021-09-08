8b8e5198a9da89c3bd67f5f29ca3978b076aa65f
Auh3b <ngaiyayec@yahoo.com>
2021-09-08 13:46:15 +0200

    removeds exposed api keys

--- store/index.js
+++ store/index.js
@@ -1,9 +1,5 @@
 // Data import
 import mpsJson from "../static/mps.json";
-import axios from "axios";
-
-const API_KEY = "AIzaSyAbQuv6-2WSf25UWq2uMucgMbSUxyqG_f4";
-const cx = "89df4317c39d31947";
 
 export const state = () => ({
   allReps: mpsJson,
@@ -27,36 +23,6 @@ export const mutations = {
   },
   setLoading: (state, payload) => {
     state.loading = payload;
-  }
-};
-
-export const actions = {
-  // asyncronous
-  async setRepArticles(state, { name }) {
-    const res = await axios(
-      `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${cx}&q="${name}"&sort=date`
-    );
-    const data = await res.data;
-    state.commit("setLoading", false);
-    // check if any search results where retreaved
-    if (+data.searchInformation.totalResults !== 0) {
-      // if available
-      // retreave needed attributes
-      const articles = data.items.map(item => {
-        const newArticleDetails = {
-          id: item.cacheId,
-          link: item.link,
-          summary: item.snippet,
-          title: item.title.split("|")[0],
-          author: item.displayLink
-        };
-        return newArticleDetails;
-      });
-      // update the articles state
-      state.commit("setRepArticles", articles);
-    } else {
-      state.commit("setRepArticles", []);
-    }
   }
 };
 
